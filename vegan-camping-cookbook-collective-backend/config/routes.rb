3df1b2bc6b074346d2f4d46a975571856ef7e67f
mbade1 <mbade314@gmail.com>
2020-08-29 11:33:08 -0500

    Routes spacing done

--- vegan-camping-cookbook-collective-backend/config/routes.rb
+++ vegan-camping-cookbook-collective-backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:index, :show, :create, :destroy, :update]
   resources :cookbooks, only: [:index, :show, :create, :destroy]
   resources :users do 
@@ -6,13 +7,10 @@ Rails.application.routes.draw do
   end
 
   post '/cookbook' => 'cookbooks#cookbook'
-
   get '/sort_breakfast' => 'recipes#sort_breakfast'
   get '/sort_lunch' => 'recipes#sort_lunch'
   get '/sort_dinner' => 'recipes#sort_dinner'
   get '/sort_snack' => 'recipes#sort_snack'
   get '/sort_dessert' => 'recipes#sort_dessert'
   get '/sort_upvotes' => 'recipes#sort_upvotes'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
